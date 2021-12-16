6b2673f066888bc8c92d51b02eab2001f50083ac
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-12-16 12:28:46 -0800

    added a home page and navlink/routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
+import Home from './Views/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header className="header">
+          <NavLink to="/players">Players</NavLink>
+          <NavLink to="/teams">Teams</NavLink>
+        </header>
+        <Switch>
+          <Route path="/players"></Route>
+          <Route path="/teams"></Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
